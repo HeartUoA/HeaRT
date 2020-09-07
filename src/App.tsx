0ad409448ba82ddbec2f04b5660abc2123256adf
Gargi Bansal <gargi.bansal@gmail.com>
2020-09-08 08:15:11 +1200

    Ran yarn prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import Homepage from './pages/Homepage';
+import React from "react";
+import Homepage from "./pages/Homepage";
 
 // TODO: Need to set up Navigation/React Router
 
 const App: React.FC = () => {
-
-  return (
-    <Homepage/>
-  );
+  return <Homepage />;
 };
 
 export default App;
