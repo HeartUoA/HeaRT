afe05ab0b5fd8efd9180a8e1956a2afa5f3e392d
Gargi Bansal <gargi.bansal@gmail.com>
2020-09-08 14:49:26 +1200

    feat(app.tsx): setup navigation for the application
    Added in React Router Dom and set it up in the App.tsx file to set up navigation for the application
    
    BREAKING CHANGE: You will need to install react-router-dom and add any new pages into the router
    hierarchy (using appropriate router/switch/stack)
    
    feat #28, feat #31, feat #35

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Homepage from "./pages/Homepage";
-
-// TODO: Need to set up Navigation/React Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App: React.FC = () => {
-  return <Homepage />;
+  return (
+    <Router>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+    </Router>
+  );
 };
 
 export default App;
