715c7f05bbd0f7713aa1a57a39d678da057145b4
Eric Pedrido <ericwashea@gmail.com>
2020-09-11 01:13:18 +1200

    feat(front-end): add form container and input boxes
    App.tsx is modified to show CourseInfo.tsx by default for testing purposes
    
    HRT-28

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/CourseInfo";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App: React.FC = () => {
