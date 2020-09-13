581701e83bfb8a9d21f1962bb87e82a11035eaca
Daria <dder725@aucklanduni.ac.nz>
2020-09-13 20:13:18 +1200

    feat(instructions.tsx): add functioning instructions model to the homepage
    HRT-2

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Card, Slider, Typography, Image } from "antd";
 import Modal from "antd/lib/modal/Modal";
 import "../styles/Instructions.css";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 const stepOneImg = require("../assets/images/instructionsStart.JPG");
 
 interface InstructionsProps {
@@ -9,12 +10,12 @@ interface InstructionsProps {
   hide: () => void;
 }
 
-const instructionSteps = {
+const instructionSteps: { [key: number]: any } = {
   1: (
     <div>
       {" "}
-      Press Play and fill in your course details. This should be the course that
-      you would like to evaluate! 📜
+      Press <b>Play</b> and fill in your course details. This should be the
+      course that you would like to evaluate! 📜
     </div>
   ),
   2: (
@@ -27,31 +28,81 @@ const instructionSteps = {
       HeaRT game to your course.
     </div>
   ),
-  3: "test",
-  4: "test",
-  5: "test",
+  3: (
+    <div>
+      When you pick a statement you are presented with a dimension bar. You need
+      to select your stance on the dimension: how much does your course tend
+      towards one card over the other. For example, is your course more
+      teacher-managed, or is it more student-managed? 🤔 <br />
+      In future versions, you will be able to explain your choice for each
+      dimension in a text note.
+    </div>
+  ),
+  4: (
+    <div>
+      Once you went through all teaching dimensions, you can preview and edit
+      your answers before saving your chart 💾
+    </div>
+  ),
+  5: (
+    <div>
+      {" "}
+      Save your chart, and compare your answers with your colleagues to see what
+      your perspectives on the course are. Discuss the results, and play the
+      game again as needed! <br />
+      The HeaRT tool is all about sparking the conversation 🔥
+    </div>
+  ),
 };
 const Instructions: React.FC<React.PropsWithChildren<InstructionsProps>> = (
   props
 ) => {
-  const setVisibility = () => {
-    props.visible = !props.visible;
+  const [instructionsState, setInstructionsStep] = useState({ currentStep: 1 });
+  const changeStep = (changeBy: number) => {
+    setInstructionsStep({
+      ...instructionsState,
+      currentStep: instructionsState.currentStep + changeBy,
+    });
   };
+
   return (
     <Modal
-      title="Basic Modal"
+      title="Instructions"
       visible={props.visible}
       onOk={props.hide}
       onCancel={props.hide}
+      width={1000}
+      footer={null}
     >
       <div className="Instructions-Container">
-        <Card>
-          {" "}
-          <div className="Image-Container">
-            <Image width={200} src={stepOneImg} />
+        <Card bordered={false}>
+          <div className="Navigation-Container">
+            <button
+              className="Navigation-Button"
+              disabled={instructionsState.currentStep <= 1}
+              onClick={(e) => changeStep(-1)}
+            >
+              <LeftOutlined />
+            </button>
+            <Image width={400} src={stepOneImg} />
+            <button
+              className="Navigation-Button"
+              disabled={
+                instructionsState.currentStep ===
+                Object.keys(instructionSteps).length
+              }
+              onClick={(e) => changeStep(+1)}
+            >
+              <RightOutlined />
+            </button>
           </div>
-          <Typography>{instructionSteps[1]}</Typography>
+          <Typography>
+            {instructionSteps[instructionsState.currentStep]}
+          </Typography>
         </Card>
+        <Typography>
+          {instructionsState.currentStep}/{Object.keys(instructionSteps).length}
+        </Typography>
       </div>
     </Modal>
   );
