e75a520975d0bef673c6cd7f7b43bef5667af39c
Daria <dder725@aucklanduni.ac.nz>
2020-09-13 20:30:53 +1200

    feat(instructions.tsx): add new screenshot, refactor the way images are retrieved
    HRT-2

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
-import { Card, Slider, Typography, Image } from "antd";
+import { Card, Typography, Image } from "antd";
 import Modal from "antd/lib/modal/Modal";
 import "../styles/Instructions.css";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 const stepOneImg = require("../assets/images/instructionsStart.JPG");
+const stepTwoImg = require("../assets/images/instructionsPickCard.JPG");
 
 interface InstructionsProps {
   visible: boolean;
   hide: () => void;
 }
 
-const instructionSteps: { [key: number]: any } = {
-  1: (
+const instructionSteps: { [key: number]: any[] } = {
+  1: [
     <div>
       {" "}
       Press <b>Play</b> and fill in your course details. This should be the
       course that you would like to evaluate! 📜
-    </div>
-  ),
-  2: (
+    </div>,
+    stepOneImg,
+  ],
+  2: [
     <div>
       You will be presented with the first pair of cards. If the statements on
       the cards are applicable to your course, pick one that resonates with you
@@ -26,9 +28,10 @@ const instructionSteps: { [key: number]: any } = {
       can always skip to the next pair! ⏭️ <br /> Psst🤫...in the future
       versions we will add the ability to edit statements, so you can tailor the
       HeaRT game to your course.
-    </div>
-  ),
-  3: (
+    </div>,
+    stepTwoImg,
+  ],
+  3: [
     <div>
       When you pick a statement you are presented with a dimension bar. You need
       to select your stance on the dimension: how much does your course tend
@@ -36,23 +39,26 @@ const instructionSteps: { [key: number]: any } = {
       teacher-managed, or is it more student-managed? 🤔 <br />
       In future versions, you will be able to explain your choice for each
       dimension in a text note.
-    </div>
-  ),
-  4: (
+    </div>,
+    stepOneImg,
+  ],
+  4: [
     <div>
       Once you went through all teaching dimensions, you can preview and edit
       your answers before saving your chart 💾
-    </div>
-  ),
-  5: (
+    </div>,
+    stepTwoImg,
+  ],
+  5: [
     <div>
       {" "}
       Save your chart, and compare your answers with your colleagues to see what
       your perspectives on the course are. Discuss the results, and play the
       game again as needed! <br />
       The HeaRT tool is all about sparking the conversation 🔥
-    </div>
-  ),
+    </div>,
+    stepOneImg,
+  ],
 };
 const Instructions: React.FC<React.PropsWithChildren<InstructionsProps>> = (
   props
@@ -84,7 +90,10 @@ const Instructions: React.FC<React.PropsWithChildren<InstructionsProps>> = (
             >
               <LeftOutlined />
             </button>
-            <Image width={400} src={stepOneImg} />
+            <Image
+              width={400}
+              src={instructionSteps[instructionsState.currentStep][1]}
+            />
             <button
               className="Navigation-Button"
               disabled={
@@ -97,7 +106,7 @@ const Instructions: React.FC<React.PropsWithChildren<InstructionsProps>> = (
             </button>
           </div>
           <Typography>
-            {instructionSteps[instructionsState.currentStep]}
+            {instructionSteps[instructionsState.currentStep][0]}
           </Typography>
         </Card>
         <Typography>
