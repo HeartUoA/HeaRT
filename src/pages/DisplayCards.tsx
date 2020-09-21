74dc225e64ec1fe400cdf29d4f5f2a5ead790a04
Mitchell Faulconbridge <42291691+mitchfaulcon@users.noreply.github.com>
2020-09-21 15:56:30 +1200

    feat(frontend): added edit card description functionality (#10)
    * fix(homepage.tsx & .css): remove underline from link on homepage
    
    * feat(displaycards): added ability to edit card descriptions
    
    Card descriptions can be edited by clicking the icon on the card to toggle between card & editing
    
    BREAKING CHANGE: This functionality uses the react-tooltip library so need to `yarn add
    react-tooltip` before starting the app
    
    * style(displaycards.tsx): moved text to a single line
    
    * refactor(displaycards.tsx): changed to use seperate state hooks for left & right cards
    
    * refactor(displaycard.tsx): switched from react-tooltip to antd tooltips
    
    * feat(displaycards.tsx,.css): changed edit icon to save icon when card is being edited
    
    * feat(displaycards.tsx,.css): added ability to cancel editing of card

--- src/pages/DisplayCards.tsx
+++ src/pages/DisplayCards.tsx
@@ -1,24 +1,75 @@
 import React, { useState } from "react";
 
-import { Card, Button, Typography, Progress } from "antd";
+import { Card, Button, Typography, Tooltip, Progress } from "antd";
 import Header from "../components/Header";
 
+import edit from "../assets/images/edit.svg";
+import save from "../assets/images/save.png";
+import cancel from "../assets/images/cancel.png";
 import "../styles/DisplayCards.css";
 import "../styles/Navigation.css";
 
-const initialValues = {
-  text: "Lorem ipsm dolor sit amet, consectetuer adipiscing elit, sed diam",
+export enum CardSide {
+  Left,
+  Right,
+}
+
+type Card = {
+  text: string;
+  isEditing: boolean;
+};
+
+const initialLeftCard: Card = {
+  text:
+    "(Left Card) Lorem ipsm dolor sit amet, consectetuer adipiscing elit, sed diam",
+  isEditing: false,
+};
+
+const initialRightCard: Card = {
+  text:
+    "(Right Card) Lorem ipsm dolor sit amet, consectetuer adipiscing elit, sed diam",
+  isEditing: false,
 };
 
 const DisplayCards: React.FC = () => {
-  const [state] = useState(initialValues);
-
+  const [leftState, setLeftState] = useState(initialLeftCard);
+  const [rightState, setRightState] = useState(initialRightCard);
   const onBackClick = () => {
     // TODO: Write code here to redirect to course info screen or to previous card
   };
 
   const onSkipClick = () => {
     // TODO: Write code here to redirect to the next card or another incomplete card
+  };
+
+  const onEditClick = (side: CardSide, cancel: boolean) => {
+    let textElement;
+    switch (side) {
+      case CardSide.Left:
+        textElement = document.getElementById("leftCardEdit");
+        if (!!textElement && !cancel) {
+          setLeftState({
+            text: textElement.innerText,
+            isEditing: !leftState.isEditing,
+          });
+        } else {
+          setLeftState({ ...leftState, isEditing: !leftState.isEditing });
+        }
+        break;
+      case CardSide.Right:
+        textElement = document.getElementById("rightCardEdit");
+        if (!!textElement && !cancel) {
+          setRightState({
+            text: textElement.innerText,
+            isEditing: !rightState.isEditing,
+          });
+        } else {
+          setRightState({ ...rightState, isEditing: !rightState.isEditing });
+        }
+        break;
+      default:
+        break;
+    }
   };
 
   const progressMade = { completed: 8, total: 14 };
@@ -32,10 +83,78 @@ const DisplayCards: React.FC = () => {
           <Typography className="Statement">Pick one statement</Typography>
           <div className="Cards-Container">
             <Card className="Card">
-              <p className="Card-Text">{state.text}</p>
+              <Tooltip
+                title={
+                  leftState.isEditing ? "Save Edited Card" : "Edit Card Text"
+                }
+                mouseEnterDelay={0.05}
+              >
+                <img
+                  src={leftState.isEditing ? save : edit}
+                  className={leftState.isEditing ? "Save" : "Edit"}
+                  alt="edit"
+                  onClick={() => onEditClick(CardSide.Left, false)}
+                />
+              </Tooltip>
+              {leftState.isEditing ? (
+                <>
+                  <Tooltip title={"Cancel Editing"} mouseEnterDelay={0.05}>
+                    <img
+                      src={cancel}
+                      className="Cancel"
+                      alt="cancel"
+                      onClick={() => onEditClick(CardSide.Left, true)}
+                    />
+                  </Tooltip>
+                  <div
+                    id="leftCardEdit"
+                    className="TextInput"
+                    contentEditable="true"
+                    suppressContentEditableWarning={true}
+                  >
+                    {leftState.text}
+                  </div>
+                </>
+              ) : (
+                <p className="Card-Text">{leftState.text}</p>
+              )}
             </Card>
             <Card className="Card">
-              <p className="Card-Text">{state.text}</p>
+              <Tooltip
+                title={
+                  rightState.isEditing ? "Save Edited Card" : "Edit Card Text"
+                }
+                mouseEnterDelay={0.05}
+              >
+                <img
+                  src={rightState.isEditing ? save : edit}
+                  className={rightState.isEditing ? "Save" : "Edit"}
+                  alt="edit"
+                  onClick={() => onEditClick(CardSide.Right, false)}
+                />
+              </Tooltip>
+              {rightState.isEditing ? (
+                <>
+                  <Tooltip title={"Cancel Editing"} mouseEnterDelay={0.05}>
+                    <img
+                      src={cancel}
+                      className="Cancel"
+                      alt="cancel"
+                      onClick={() => onEditClick(CardSide.Right, true)}
+                    />
+                  </Tooltip>
+                  <div
+                    id="rightCardEdit"
+                    className="TextInput"
+                    contentEditable="true"
+                    suppressContentEditableWarning={true}
+                  >
+                    {rightState.text}
+                  </div>
+                </>
+              ) : (
+                <p className="Card-Text">{rightState.text}</p>
+              )}
             </Card>
           </div>
         </div>
