8a7fa827bf39681cf1adeed3ffafaeb851d571e9
Preet Patel <iam@preetpatel.com>
2020-09-26 15:01:18 +1200

    fix: add tests

--- server/course/course.test.js
+++ server/course/course.test.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import request from "supertest";
 import app from "../app";
 import Course from "./course.model";
+import User from "../users/user.model";
 
 describe("/api/course tests", () => {
   const mongod = new MongodbMemoryServer();
@@ -10,9 +11,29 @@ describe("/api/course tests", () => {
   beforeAll(async () => {
     const uri = await mongod.getConnectionString();
     await mongoose.connect(uri, { useNewUrlParser: true });
+
+    const postResponse = await request(app).post("/api/users").send({
+      name: "John Doe",
+      passwordHash: "aMoreSecureHashThanThisPlease",
+      email: "john@doe.com",
+      position: "Course Cordinator",
+      department: "Engineering",
+      institution: "The University Of Auckland",
+      username: "jdoe",
+    });
+    expect(postResponse.status).toBe(200);
+
+    const authResponse = await request(app)
+      .post("/api/users/authenticate/jdoe")
+      .send({
+        passwordHash: "aMoreSecureHashThanThisPlease",
+      });
+    expect(authResponse.status).toBe(200);
+    expect(authResponse.body.accessToken).toBeDefined();
   });
 
   afterAll(async () => {
+    await User.remove({});
     await mongoose.disconnect();
     await mongod.stop();
   });
@@ -22,22 +43,29 @@ describe("/api/course tests", () => {
   });
 
   it("should post and get courses", async () => {
-    const dateNow = new Date();
+    const authResponse = await request(app)
+      .post("/api/users/authenticate/jdoe")
+      .send({
+        passwordHash: "aMoreSecureHashThanThisPlease",
+      });
+    expect(authResponse.status).toBe(200);
 
-    const postResponse = await request(app).post("/api/course").send({
-      name: "SOFTENG 700",
-      date: dateNow,
-      cohortSize: 200,
-      role: "Course Cordinator",
-      ageOfCourse: 5,
-      createdByUserID: "testID",
-    });
+    const postResponse = await request(app)
+      .post("/api/course")
+      .set("Authorization", "Bearer " + authResponse.body.accessToken)
+      .send({
+        name: "SOFTENG 700",
+        cohortSize: 200,
+        role: "Course Cordinator",
+        ageOfCourse: 5,
+      });
+
     expect(postResponse.status).toBe(200);
     expect(postResponse.body).toBeDefined();
 
-    const getResponse = await request(app).get(
-      "/api/course/" + postResponse.body._id
-    );
+    const getResponse = await request(app)
+      .get("/api/course/" + postResponse.body._id)
+      .set("Authorization", "Bearer " + authResponse.body.accessToken);
     expect(getResponse.status).toBe(200);
     expect(getResponse.body).toEqual([
       expect.objectContaining({
@@ -45,11 +73,8 @@ describe("/api/course tests", () => {
         cohortSize: 200,
         role: "Course Cordinator",
         ageOfCourse: 5,
-        createdByUserID: "testID",
       }),
     ]);
-    // Need to check date seperately after casting to a Date object
-    expect(new Date(getResponse.body[0].date)).toEqual(dateNow);
     expect(getResponse.body[0]).toHaveProperty("_id");
   });
 });
