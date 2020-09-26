2a9f43372484df0ae4ca91a21478508a904c4545
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2020-09-26 15:38:45 +1200

    feat(courseinfo.tsx,.css): added routing from course info & made fields required

--- src/pages/CourseInfo.tsx
+++ src/pages/CourseInfo.tsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 
-import { Button, Input, Layout, Menu, Select, Typography } from "antd";
+import { Button, Input, Layout, Select, Typography, Form } from "antd";
 
 import Header from "../components/Header";
 import "../styles/CourseInfo.css";
 
 const { Option } = Select;
 
-const CourseInfo: React.FC = () => {
+const CourseInfo: React.FC<RouteComponentProps> = (props) => {
   const onConfirmClick = () => {
-    // TODO: Write code here to redirect to cards screen
+    // Redirect to cards screen
+    props.history.push("/DisplayCards");
   };
 
   const onCancelClick = () => {
-    // TODO: Write code here to redirect back to homepage
+    // Redirect back to homepage
+    props.history.push("/");
   };
 
   return (
@@ -21,75 +24,95 @@ const CourseInfo: React.FC = () => {
       <Header />
 
       <Layout.Content className="Main-Container">
-        <div className="Form-Container">
+        <Form className="Form-Container" onFinish={onConfirmClick}>
           <Typography className="Create-Chart-Text">Create Chart</Typography>
           <div>
             <div>
               <Typography className="Form-Text">Course Name</Typography>
-              <Input
-                className="Form-Input"
-                name="courseName"
-                placeholder="e.g SOFTENG 761"
-              />
+              <Form.Item
+                name="courseNameFormItem"
+                rules={[{ required: true, message: "Course Name Required" }]}
+              >
+                <Input
+                  className="Form-Input"
+                  name="courseName"
+                  placeholder="e.g SOFTENG 761"
+                />
+              </Form.Item>
             </div>
             <div>
               <Typography className="Form-Text">Your Role in Course</Typography>
-              <Input
-                className="Form-Input"
-                name="courseRole"
-                placeholder="e.g Course Coordinator"
-              />
+              <Form.Item
+                name="courseRoleFormItem"
+                rules={[{ required: true, message: "Course Role Required" }]}
+              >
+                <Input
+                  className="Form-Input"
+                  name="courseRole"
+                  placeholder="e.g Course Coordinator"
+                />
+              </Form.Item>
             </div>
           </div>
           <div className="Select-Form">
             <div>
               <Typography className="Form-Text">Course Age</Typography>
-              <Select className="Selector" placeholder="Select">
-                <Option value="new">
-                  <span className="Selection-Text">New course</span>
-                </Option>
-                <Option value="three">
-                  <span className="Selection-Text">3 years or less</span>
-                </Option>
-                <Option value="more">
-                  <span className="Selection-Text">More than 3 years</span>
-                </Option>
-              </Select>
+              <Form.Item
+                className="Selector"
+                name="courseAgeFormItem"
+                rules={[{ required: true, message: "Course Age Required" }]}
+              >
+                <Select placeholder="Select">
+                  <Option value="new">
+                    <span className="Selection-Text">New course</span>
+                  </Option>
+                  <Option value="three">
+                    <span className="Selection-Text">3 years or less</span>
+                  </Option>
+                  <Option value="more">
+                    <span className="Selection-Text">More than 3 years</span>
+                  </Option>
+                </Select>
+              </Form.Item>
             </div>
             <div className="Cohort-Size-Label">
               <Typography className="Form-Text">Cohort Size</Typography>
-              <Select className="Selector" placeholder="Select">
-                <Option value="less">
-                  <span className="Selection-Text Selection-Text-Right">
-                    Less than 20
-                  </span>
-                </Option>
-                <Option value="twenty">
-                  <span className="Selection-Text Selection-Text-Right">
-                    20-49
-                  </span>
-                </Option>
-                <Option value="fifty">
-                  <span className="Selection-Text Selection-Text-Right">
-                    50-100
-                  </span>
-                </Option>
-                <Option value="hundred">
-                  <span className="Selection-Text Selection-Text-Right">
-                    More than 100
-                  </span>
-                </Option>
-              </Select>
+              <Form.Item
+                className="Selector"
+                name="cohortSizeFormItem"
+                rules={[{ required: true, message: "Cohort Size Required" }]}
+              >
+                <Select placeholder="Select">
+                  <Option value="less">
+                    <span className="Selection-Text Selection-Text-Right">
+                      Less than 20
+                    </span>
+                  </Option>
+                  <Option value="twenty">
+                    <span className="Selection-Text Selection-Text-Right">
+                      20-49
+                    </span>
+                  </Option>
+                  <Option value="fifty">
+                    <span className="Selection-Text Selection-Text-Right">
+                      50-100
+                    </span>
+                  </Option>
+                  <Option value="hundred">
+                    <span className="Selection-Text Selection-Text-Right">
+                      More than 100
+                    </span>
+                  </Option>
+                </Select>
+              </Form.Item>
             </div>
           </div>
           <div className="Button-Container">
-            <Button
-              type="primary"
-              className="Nav-Button Cancel-Button"
-              onClick={onConfirmClick}
-            >
-              <Typography className="Button-Font">Confirm</Typography>
-            </Button>
+            <Form.Item className="Confirm-Button">
+              <Button type="primary" htmlType="submit" className="Nav-Button">
+                <Typography className="Button-Font">Confirm</Typography>
+              </Button>
+            </Form.Item>
             <Button
               type="primary"
               className="Nav-Button"
@@ -98,10 +121,10 @@ const CourseInfo: React.FC = () => {
               <Typography className="Button-Font">Cancel</Typography>
             </Button>
           </div>
-        </div>
+        </Form>
       </Layout.Content>
     </div>
   );
 };
 
-export default CourseInfo;
+export default withRouter(CourseInfo);
