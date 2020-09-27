c7c8171670d7c535fe9d2163e1bf5920abca8446
Michelle Tan <37464749+michellemtan@users.noreply.github.com>
2020-09-27 15:05:39 +1300

    feat(dimensions): add display slider (#20)
    * feat(display cards component): add dimension component to display cards page
    
    BREAKING CHANGE: Changed dimension component to style depending whether on preview page or display
    page
    
    * feat(displaycards): change button to next when card is selected
    
    * feat(displaycards): change prompt when card is selected
    
    * feat(dimension): lucy to help fix
    
    * fix(dimension): slider updates parent component state
    
    * feat(display cards): change selected card to reflect slider values
    
    * feat(displaycards css): style dimension and cards when cards is clicked
    
    * feat(dimension): add markers so 3rd dimension can be added
    
    n
    
    * feat(dimension): remove unnecessary scale labels
    
    n
    
    * feat(dimension): add styling to slider bar
    
    n
    
    * fix(display cards): edit cards no longer conflicts with clicking cards event handler
    
    * feat(dimension and displaycards): gradient for cards when dimension position value changes
    
    * fix(dimension): increase dimension card size
    
    Co-authored-by: lucycy98 <lucy.chen.nz@gmail.com>
    Co-authored-by: gargi-bansal <42624632+gargi-bansal@users.noreply.github.com>

--- src/pages/DisplayCards.tsx
+++ src/pages/DisplayCards.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import { Card, Button, Typography, Tooltip, Progress } from "antd";
 import Header from "../components/Header";
+import Dimension from "../components/Dimension";
 
 import edit from "../assets/images/edit.svg";
 import save from "../assets/images/save.png";
@@ -16,24 +17,54 @@ export enum CardSide {
 
 type Card = {
   text: string;
+  isSelected: boolean;
   isEditing: boolean;
 };
 
 const initialLeftCard: Card = {
   text:
     "(Left Card) Lorem ipsm dolor sit amet, consectetuer adipiscing elit, sed diam",
+  isSelected: false,
   isEditing: false,
 };
 
 const initialRightCard: Card = {
   text:
     "(Right Card) Lorem ipsm dolor sit amet, consectetuer adipiscing elit, sed diam",
+  isSelected: false,
   isEditing: false,
+};
+
+const tempDimension = {
+  dimensionValue: "Dimension",
+  scale: 100,
+  userExplanation: "This is a user explanation.",
+  isPreview: false,
+  marks: {
+    0: "Fixed",
+    30: "A 3rd dimension",
+    100: {
+      style: {
+        color: "#ef547f",
+      },
+      label: <strong>Active</strong>,
+    },
+  },
+};
+
+const tempColours = {
+  leftCardColour: "#FFFFFF",
+  rightCardColour: "#FFFFFF",
 };
 
 const DisplayCards: React.FC = () => {
   const [leftState, setLeftState] = useState(initialLeftCard);
   const [rightState, setRightState] = useState(initialRightCard);
+  const [colours, setColours] = useState(tempColours);
+  const [dimension, setDimension] = useState(tempDimension);
+  const progressMade = { completed: 8, total: 14 };
+  let isCardSelected = leftState.isSelected || rightState.isSelected;
+
   const onBackClick = () => {
     // TODO: Write code here to redirect to course info screen or to previous card
   };
@@ -42,7 +73,36 @@ const DisplayCards: React.FC = () => {
     // TODO: Write code here to redirect to the next card or another incomplete card
   };
 
-  const onEditClick = (side: CardSide, cancel: boolean) => {
+  const onNextClick = () => {
+    // TODO: Write code here to redirect to the next card or another incomplete card
+  };
+
+  const onCardClick = (side: CardSide) => {
+    switch (side) {
+      case CardSide.Left:
+        setLeftState({ ...leftState, isSelected: true });
+        setRightState({ ...rightState, isSelected: false });
+        onDimensionChange(0);
+        break;
+
+      case CardSide.Right:
+        setRightState({ ...rightState, isSelected: true });
+        setLeftState({ ...leftState, isSelected: false });
+        onDimensionChange(100);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const onEditClick = (
+    event: React.MouseEvent,
+    side: CardSide,
+    cancel: boolean
+  ) => {
+    event.stopPropagation();
+
     let textElement;
     switch (side) {
       case CardSide.Left:
@@ -51,6 +111,7 @@ const DisplayCards: React.FC = () => {
           setLeftState({
             text: textElement.innerText,
             isEditing: !leftState.isEditing,
+            isSelected: leftState.isSelected,
           });
         } else {
           setLeftState({ ...leftState, isEditing: !leftState.isEditing });
@@ -62,6 +123,7 @@ const DisplayCards: React.FC = () => {
           setRightState({
             text: textElement.innerText,
             isEditing: !rightState.isEditing,
+            isSelected: rightState.isSelected,
           });
         } else {
           setRightState({ ...rightState, isEditing: !rightState.isEditing });
@@ -72,17 +134,57 @@ const DisplayCards: React.FC = () => {
     }
   };
 
-  const progressMade = { completed: 8, total: 14 };
+  const onDimensionChange = (value: number) => {
+    // Change selected card to reflect slider values
+    if (value < 50) {
+      setLeftState({ ...leftState, isSelected: true });
+      setRightState({ ...rightState, isSelected: false });
+    } else {
+      setRightState({ ...rightState, isSelected: true });
+      setLeftState({ ...leftState, isSelected: false });
+    }
+
+    setDimension({ ...dimension, scale: value });
+    setColours({
+      leftCardColour: getLeftColour(value),
+      rightCardColour: getRightColour(value),
+    });
+  };
+
+  function getLeftColour(value: number) {
+    var hue = 344.7;
+    var value = 87 + (13 / 100) * value;
+    console.log("left" + value);
+    return ["hsl(", hue, ",100%,", value, "%)"].join("");
+  }
+
+  function getRightColour(value: number) {
+    var hue = 344.7;
+    var value = 100 - (13 / 100) * value;
+    console.log("right" + value);
+    return ["hsl(", hue, ",100%,", value, "%)"].join("");
+  }
 
   return (
     <div className="DisplayCards">
       <Header />
-
       <div className="Content">
         <div>
-          <Typography className="Statement">Pick one statement</Typography>
+          {isCardSelected ? (
+            <Typography className="Statement">
+              Which statement describes SOFTENG761 best?
+            </Typography>
+          ) : (
+            <Typography className="Statement">Pick one statement</Typography>
+          )}
           <div className="Cards-Container">
-            <Card className="Card">
+            <Card
+              className={`${isCardSelected ? "Card-Clicked" : "Card"} ${
+                leftState.isSelected ? "Card-Selected" : ""
+              }`}
+              onClick={() => onCardClick(CardSide.Left)}
+              style={{ backgroundColor: colours.leftCardColour }}
+            >
               <Tooltip
                 title={
                   leftState.isEditing ? "Save Edited Card" : "Edit Card Text"
@@ -93,7 +195,7 @@ const DisplayCards: React.FC = () => {
                   src={leftState.isEditing ? save : edit}
                   className={leftState.isEditing ? "Save" : "Edit"}
                   alt="edit"
-                  onClick={() => onEditClick(CardSide.Left, false)}
+                  onClick={(event) => onEditClick(event, CardSide.Left, false)}
                 />
               </Tooltip>
               {leftState.isEditing ? (
@@ -103,7 +205,9 @@ const DisplayCards: React.FC = () => {
                       src={cancel}
                       className="Cancel"
                       alt="cancel"
-                      onClick={() => onEditClick(CardSide.Left, true)}
+                      onClick={(event) =>
+                        onEditClick(event, CardSide.Left, true)
+                      }
                     />
                   </Tooltip>
                   <div
@@ -119,7 +223,13 @@ const DisplayCards: React.FC = () => {
                 <p className="Card-Text">{leftState.text}</p>
               )}
             </Card>
-            <Card className="Card">
+            <Card
+              className={`${isCardSelected ? "Card-Clicked" : "Card"} ${
+                rightState.isSelected ? "Card-Selected" : ""
+              }`}
+              onClick={() => onCardClick(CardSide.Right)}
+              style={{ backgroundColor: colours.rightCardColour }}
+            >
               <Tooltip
                 title={
                   rightState.isEditing ? "Save Edited Card" : "Edit Card Text"
@@ -130,7 +240,7 @@ const DisplayCards: React.FC = () => {
                   src={rightState.isEditing ? save : edit}
                   className={rightState.isEditing ? "Save" : "Edit"}
                   alt="edit"
-                  onClick={() => onEditClick(CardSide.Right, false)}
+                  onClick={(event) => onEditClick(event, CardSide.Right, false)}
                 />
               </Tooltip>
               {rightState.isEditing ? (
@@ -140,7 +250,9 @@ const DisplayCards: React.FC = () => {
                       src={cancel}
                       className="Cancel"
                       alt="cancel"
-                      onClick={() => onEditClick(CardSide.Right, true)}
+                      onClick={(event) =>
+                        onEditClick(event, CardSide.Right, true)
+                      }
                     />
                   </Tooltip>
                   <div
@@ -157,6 +269,13 @@ const DisplayCards: React.FC = () => {
               )}
             </Card>
           </div>
+          {isCardSelected ? (
+            <Dimension
+              {...{ dimension: dimension, sliderUpdate: onDimensionChange }}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </div>
       <div className="Navigation">
@@ -192,13 +311,23 @@ const DisplayCards: React.FC = () => {
             strokeWidth={20}
           />
         </div>
-        <Button
-          type="primary"
-          className="NavigationButton"
-          onClick={onSkipClick}
-        >
-          <Typography className="Navigation-Button-Text">Skip</Typography>
-        </Button>
+        {isCardSelected ? (
+          <Button
+            type="primary"
+            className="NavigationButton"
+            onClick={onNextClick}
+          >
+            <Typography className="Navigation-Button-Text">Next</Typography>
+          </Button>
+        ) : (
+          <Button
+            type="primary"
+            className="NavigationButton"
+            onClick={onSkipClick}
+          >
+            <Typography className="Navigation-Button-Text">Skip</Typography>
+          </Button>
+        )}
       </div>
     </div>
   );
