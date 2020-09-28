4b93d244a66d82704df044c265a95fa0c63e2e66
gargi-bansal <42624632+gargi-bansal@users.noreply.github.com>
2020-09-29 12:08:50 +1300

    feat(frontend): link pages and fix common styling

--- src/components/Dimension.tsx
+++ src/components/Dimension.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 import { Card, Slider, Typography } from "antd";
+import TextArea from "antd/lib/input/TextArea";
 import "../styles/Dimension.css";
 
 interface DimensionProps {
@@ -14,6 +15,7 @@ interface DimensionProps {
 interface MainProps {
   dimension: DimensionProps;
   sliderUpdate: (value: number) => void;
+  userExplanationUpdate?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
 interface SliderMarks {
@@ -30,20 +32,30 @@ const Dimension: React.FC<MainProps> = (props: MainProps) => {
     <Card
       className={props.dimension.isPreview ? "Card-Preview" : "Card-Dimension"}
     >
-      <p className="Card-Text">{props.dimension.dimensionValue}</p>
-      <div className="Slider">
+      <p className="Dimension-Name-Text">{props.dimension.dimensionValue}</p>
         <Slider
-          className="Slider-Bar"
+          className="Slider"
           value={props.dimension.scale}
           onChange={props.sliderUpdate}
           marks={props.dimension.marks}
+          tooltipVisible={false}
+          included={false}
         />
+        {props.dimension.isPreview ? (
         <Typography className="User-Explanation">
           {props.dimension.userExplanation.length > 0
             ? '"' + props.dimension.userExplanation + '"'
             : ""}
-        </Typography>
-      </div>
+      </Typography>
+      ) : (
+        <TextArea
+          className="User-Explanation-Edit"
+          value={props.dimension.userExplanation}
+          onChange={props.userExplanationUpdate}
+          placeholder="Explain your reasoning for your stance here..."
+          rows={3}
+        />
+      )}
     </Card>
   );
 };
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { withRouter, RouteComponentProps} from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { Button } from "antd";
+
+import Header from "../components/Header";
+import Instructions from "../components/Instructions"
+import "../styles/Dashboard.css";
+
+const Dashboard: React.FC<RouteComponentProps> = (props) => {
+  const [ cookies ] = useCookies(['accessToken']);
+  const [showInstructions, setShowInstructions] = useState(false);
+
+  useEffect(() => {
+    if (!cookies['accessToken']) {
+      props.history.push("/Login");
+    }
+  }, [cookies]);
+
+  const onInstructionsClick = () => {
+    setShowInstructions(!showInstructions);
+  };
+
+  const createNewCourse = () => {
+    props.history.push("/CreateCourse");
+  }
+
+  return (
+    <div className="Dashboard">
+      <Header />
+      <Instructions visible={showInstructions} hide={onInstructionsClick} />
+      <div className="Dashboard-Content">
+        <Button onClick={onInstructionsClick}>How to play</Button>
+        <Button onClick={createNewCourse}>Create new course</Button>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(Dashboard);
