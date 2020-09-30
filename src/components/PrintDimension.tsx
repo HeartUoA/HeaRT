de9467fc509960ee9e46bdcf0ffbe9ad1673d7e9
TCHE614 <48304096+TCHE614@users.noreply.github.com>
2020-09-30 15:47:02 +1300

    npm bugging out

--- src/components/PrintDimension.tsx
+++ src/components/PrintDimension.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Slider, Switch } from "antd";
 import React, { useEffect, useRef, useState } from "react";
 import "../styles/PrintDimension.css";
@@ -53,3 +54,31 @@ const PrintDimension: React.FC<React.PropsWithChildren<PrintDimensionProps>> = (
 };
 
 export default PrintDimension;
+=======
+import { Slider, Switch } from 'antd';
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import { RouteComponentProps } from 'react-router-dom';
+import "../styles/PrintDimension.css";
+
+
+const PrintDimension: React.FC = () => {
+
+    return (
+      <>
+        <span className="printDimensionText">Pedagogical Dimension:</span>
+        <span className="printTitleText">Nature & purpose of learning tasks</span>
+        <span className="printContinuumText">Continuum:</span>
+        <div className="ContiuumStatements">
+            <span className="printSliderText">Academic/Abstract</span>
+            <span className="printSliderText">Authentic/experimental</span>
+        </div>
+        <Slider className="printSlider" range defaultValue={[0, 100]} disabled={true} />
+        <span className="printStatementText">Statement reflects 'Academic/abstract' purpose</span>
+      </>
+    );
+  
+}
+
+export default PrintDimension;
+>>>>>>> ad928bc... npm bugging out
--- src/pages/PrintCards.tsx
+++ src/pages/PrintCards.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useRef } from "react";
 import { useCookies } from "react-cookie";
 import { withRouter, RouteComponentProps } from "react-router-dom";
@@ -92,6 +93,91 @@ const PrintCards: React.FC<RouteComponentProps> = (props) => {
       .catch((e) => console.log(e));
   }, [cookies]);
 
+=======
+import React, { constructor, useEffect, useRef, useState } from "react";
+import { useCookies } from "react-cookie";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import { Button, Input, Row, Typography } from "antd";
+import Header from "../components/Header";
+import Dimension from "../components/PrintDimension";
+import "../styles/PrintCards.css";
+import { API_DOMAIN } from "../config";
+
+type statementCard = {
+  text: string;
+};
+
+const initialStatementCard: statementCard = {
+  text: "Lorem ipsm dolor sit amet, consectetuer adipiscing elit, sed diam",
+};
+
+const chartID = "5f73b99d5d6eec214825e006";
+
+//dummy data
+type DimensionCard = {
+  dimensionName: string;
+  leftContinuum: string;
+  rightContinuum: string;
+  statementReflection: string;
+};
+
+const tempValues = [
+    {
+      dimensionValue: "Dimension1",
+    },
+    {
+      dimensionValue: "Dimension2",
+    },
+    {
+      dimensionValue: "Dimension3",
+    },
+    {
+      dimensionValue: "Dimension4",
+    },
+    {
+      dimensionValue: "Dimension5",
+    },
+    {
+      dimensionValue: "Dimension6",
+    },
+    {
+      dimensionValue: "Dimension7",
+    },
+    {
+      dimensionValue: "Dimension8",
+    },
+  ];
+
+const PrintCards: React.FC<RouteComponentProps> = (props) => {
+  const [cookies] = useCookies(["accessToken"]);
+  const [leftState, setLeftState] = useState(initialStatementCard);
+  const [ dimensions, setDimensions ] = useState(tempValues);
+  let num =1;
+  useEffect(() => {
+    if (!cookies["accessToken"]) {
+      props.history.push("/Login");
+    }    
+
+    fetch(`${API_DOMAIN}dimensions/forchart/${chartID}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI1ZjcxODRjOThhYWQ1MzUxOTg1ZDJkMzAiLCJpYXQiOjE2MDEyNzUxMDh9.0MN9vV7WRSc-m5hpt3t8mJakVNElJHe4a2fuc1-aFcs",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setLeftState({
+            text: res[0].leftCardStatement,
+            // text:JSON.stringify(res),
+          });
+          console.log(res);
+        })
+        .catch((e) => console.log(e));
+  }, [cookies]);
+  
+>>>>>>> ad928bc... npm bugging out
   const onBackClick = async (): Promise<void> => {
     props.history.push("/Dashboard");
   };
@@ -103,6 +189,7 @@ const PrintCards: React.FC<RouteComponentProps> = (props) => {
       </div>
       <div className="PrintCardsContainer">
         <div className="PrintCardsContent">
+<<<<<<< HEAD
           <ComponentToPrint ref={componentRef} />
         </div>
       </div>
@@ -120,9 +207,32 @@ const PrintCards: React.FC<RouteComponentProps> = (props) => {
             content={() => componentRef.current}
           />
         </Button>
+=======
+          <Row>
+            {dimensions.map((item) => (
+                <><div className="PrintingCards">
+                    <span className="Print-Card-Text" id={num} >{leftState.text}</span>
+                </div>
+                <div className="PrintingCards">
+                    <Dimension />
+                </div>
+                </>
+            ))}
+          </Row>
+        </div>
+      </div>
+      <div className="BackButtonDiv">
+        <Button type="primary" className="Back-Button" onClick={onBackClick}>
+          Back
+        </Button>
+>>>>>>> ad928bc... npm bugging out
       </div>
     </div>
   );
 };
 
+<<<<<<< HEAD
 export default withRouter(PrintCards);
+=======
+export default withRouter(PrintCards);
+>>>>>>> ad928bc... npm bugging out
