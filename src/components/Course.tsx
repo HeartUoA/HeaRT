78d30963c56ae619c0744dfef0e0abed6c94cd1e
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2020-09-30 18:41:43 +1300

    feat(dashboard): created UI for viewing courses on the dashboard

---
+++ src/components/Course.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Card, Typography } from "antd";
+import "../styles/Course.css";
+
+interface CourseProps {
+  courseName: string;
+}
+
+const Dimension: React.FC<CourseProps> = (props: CourseProps) => {
+  const onClick = () => {
+    // TODO: Redirect to appropriate course page
+  };
+
+  return (
+    <Card className="Course-Card" onClick={onClick}>
+      <Typography className="Course-Title">{props.courseName}</Typography>
+      <div className="Course-Card-Bottom" />
+    </Card>
+  );
+};
+
+export default Dimension;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { withRouter, RouteComponentProps} from "react-router-dom";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import { Button } from "antd";
+import { Button, Row, Card, Typography } from "antd";
 
 import Header from "../components/Header";
-import Instructions from "../components/Instructions"
+import Instructions from "../components/Instructions";
+import Course from "../components/Course";
 import "../styles/Dashboard.css";
 
+import courses from "../dummyData/courses";
+import plus from "../assets/images/plus.png";
+
 const Dashboard: React.FC<RouteComponentProps> = (props) => {
-  const [ cookies ] = useCookies(['accessToken']);
+  const [cookies] = useCookies(["accessToken"]);
   const [showInstructions, setShowInstructions] = useState(false);
 
   useEffect(() => {
-    if (!cookies['accessToken']) {
+    if (!cookies["accessToken"]) {
       props.history.push("/Login");
     }
   }, [cookies]);
@@ -23,15 +27,38 @@ const Dashboard: React.FC<RouteComponentProps> = (props) => {
 
   const createNewCourse = () => {
     props.history.push("/CreateCourse");
-  }
+  };
 
   return (
     <div className="Dashboard">
       <Header />
       <Instructions visible={showInstructions} hide={onInstructionsClick} />
       <div className="Dashboard-Content">
-        <Button onClick={onInstructionsClick}>How to play</Button>
-        <Button onClick={createNewCourse}>Create new course</Button>
+        <Typography className="Courses-Heading">Courses</Typography>
+        <Button
+          type="primary"
+          className="Instructions-Button"
+          onClick={onInstructionsClick}
+        >
+          How to play
+        </Button>
+      </div>
+      <div className="Dashboard-Cards">
+        <Row className="Course-Row">
+          {courses.map((item) => {
+            return (
+              <Course
+                {...{
+                  courseName: item.name,
+                }}
+              />
+            );
+          })}
+          <Button className="Create-Course-Button" onClick={createNewCourse}>
+            <img src={plus} alt="plus" />
+            <Typography>Create New Course</Typography>
+          </Button>
+        </Row>
       </div>
     </div>
   );
