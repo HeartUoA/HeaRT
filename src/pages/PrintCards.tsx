85194c5761e308910cf66d4114dd01761da897fc
TCHE614 <48304096+TCHE614@users.noreply.github.com>
2020-10-01 11:42:26 +1300

    refactor(printcards.tsx): updated a variable to rebase with master

--- src/pages/PrintCards.tsx
+++ src/pages/PrintCards.tsx
@@ -26,61 +26,61 @@ type DimensionCard = {
 };
 
 const tempValues = [
-    {
-      dimensionValue: "Dimension1",
-    },
-    {
-      dimensionValue: "Dimension2",
-    },
-    {
-      dimensionValue: "Dimension3",
-    },
-    {
-      dimensionValue: "Dimension4",
-    },
-    {
-      dimensionValue: "Dimension5",
-    },
-    {
-      dimensionValue: "Dimension6",
-    },
-    {
-      dimensionValue: "Dimension7",
-    },
-    {
-      dimensionValue: "Dimension8",
-    },
-  ];
+  {
+    dimensionValue: "Dimension1",
+  },
+  {
+    dimensionValue: "Dimension2",
+  },
+  {
+    dimensionValue: "Dimension3",
+  },
+  {
+    dimensionValue: "Dimension4",
+  },
+  {
+    dimensionValue: "Dimension5",
+  },
+  {
+    dimensionValue: "Dimension6",
+  },
+  {
+    dimensionValue: "Dimension7",
+  },
+  {
+    dimensionValue: "Dimension8",
+  },
+];
 
 const PrintCards: React.FC<RouteComponentProps> = (props) => {
   const [cookies] = useCookies(["accessToken"]);
   const [leftState, setLeftState] = useState(initialStatementCard);
-  const [ dimensions, setDimensions ] = useState(tempValues);
-  let num =1;
+  const [dimensions, setDimensions] = useState(tempValues);
+  let num = 1;
   useEffect(() => {
     if (!cookies["accessToken"]) {
       props.history.push("/Login");
-    }    
+    }
 
     fetch(`${API_DOMAIN}dimensions/forchart/${chartID}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI1ZjcxODRjOThhYWQ1MzUxOTg1ZDJkMzAiLCJpYXQiOjE2MDEyNzUxMDh9.0MN9vV7WRSc-m5hpt3t8mJakVNElJHe4a2fuc1-aFcs",
-        },
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI1ZjcxODRjOThhYWQ1MzUxOTg1ZDJkMzAiLCJpYXQiOjE2MDEyNzUxMDh9.0MN9vV7WRSc-m5hpt3t8mJakVNElJHe4a2fuc1-aFcs",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setLeftState({
+          text: res[0].leftCardStatement,
+          // text:JSON.stringify(res),
+        });
+        console.log(res);
       })
-        .then((res) => res.json())
-        .then((res) => {
-          setLeftState({
-            text: res[0].leftCardStatement,
-            // text:JSON.stringify(res),
-          });
-          console.log(res);
-        })
-        .catch((e) => console.log(e));
+      .catch((e) => console.log(e));
   }, [cookies]);
-  
+
   const onBackClick = async (): Promise<void> => {
     props.history.push("/Dashboard");
   };
@@ -94,13 +94,14 @@ const PrintCards: React.FC<RouteComponentProps> = (props) => {
         <div className="PrintCardsContent">
           <Row>
             {dimensions.map((item) => (
-                <><div className="PrintingCards">
-                    <span className="Print-Card-Text" id={num} >{leftState.text}</span>
+              <>
+                <div className="PrintingCards">
+                  <span className="Print-Card-Text">{leftState.text}</span>
                 </div>
                 <div className="PrintingCards">
-                    <Dimension />
+                  <Dimension />
                 </div>
-                </>
+              </>
             ))}
           </Row>
         </div>
