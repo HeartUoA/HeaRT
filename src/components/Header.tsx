e930851c305d01f76142607c6cb709be5bcd3ff6
Michelle Tan <37464749+michellemtan@users.noreply.github.com>
2020-10-01 17:00:24 +1300

    feat(header): added routing and modal for clicking on header logo (#34)
    * feat(header): display modal when user is playing game and trying to exit
    
    * fix(database-model): add additional properties to model (#32)
    
    * feat(header): check if the user is playing game
    
    * fix(header): centre modal
    
    * feat(header): fix PR comments
    
    * feat(header): style modal footer buttons
    
    * Delete package-lock.json
    
    Co-authored-by: Preet Patel <iam@preetpatel.com>
    Co-authored-by: gargi-bansal <42624632+gargi-bansal@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 
-import { Typography, Layout, Button } from "antd";
+import { Typography, Layout, Button, Modal } from "antd";
 
 import logo from "../assets/images/logo.svg";
 import "../styles/Header.css";
@@ -9,21 +9,70 @@ import { withRouter, RouteComponentProps } from "react-router-dom";
 
 const Header: React.FC<RouteComponentProps> = (props) => {
   const [cookies, setCookie] = useCookies(["accessToken"]);
+  const [showExitModal, setShowExitModal] = useState(false);
 
   const onLogoutClick = () => {
     setCookie("accessToken", "");
     props.history.push("/");
   };
 
+  const onLogoClick = () => {
+    // If the user is NOT logged in, it should redirect them to the homepage
+    if (!cookies["accessToken"]) {
+      props.history.push("/");
+    } else {
+      // If the user is logged in and playing a game, it should show a confirmation dialog
+      if (props.match.path === "/DisplayCards") {
+        setShowExitModal(true);
+      } else {
+        props.history.push("/Dashboard");
+      }
+    }
+  };
+
+  const handleOk = () => {
+    setShowExitModal(false);
+    props.history.push("/Dashboard");
+  };
+
+  const handleCancel = () => {
+    setShowExitModal(false);
+  };
+
   return (
     <Layout.Header className="Header">
-      <div className="Logo-Container">
+      <div className="Logo-Container" onClick={onLogoClick}>
         <img src={logo} className="Logo" alt="logo" />
         <Typography className="Logo-Title-Header">HeaRT</Typography>
       </div>
-      {!!cookies["accessToken"] && (
+      {cookies["accessToken"] && (
         <Button onClick={onLogoutClick}>Logout</Button>
       )}
+      <Modal
+        title="Leave Game"
+        centered
+        visible={showExitModal}
+        footer={[
+          <Button
+            key="cancel"
+            type="primary"
+            onClick={handleCancel}
+            className="Modal-Button"
+          >
+            Cancel
+          </Button>,
+          <Button
+            key="ok"
+            type="primary"
+            onClick={handleOk}
+            className="Modal-Button"
+          >
+            Ok
+          </Button>,
+        ]}
+      >
+        <p>Are you sure you want to return to the dashboard?</p>
+      </Modal>
     </Layout.Header>
   );
 };
