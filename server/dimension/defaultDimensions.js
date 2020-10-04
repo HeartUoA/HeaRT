f082cde6b7dfa79be8ddaaab418b20997284e270
Gargi Bansal <gargi.bansal@gmail.com>
2020-10-04 21:59:43 +1300

    feat(back end): updated default dimensions with ones claire specified
    updated default dimensions with ones claire specified

--- server/dimension/defaultDimensions.js
+++ server/dimension/defaultDimensions.js
@@ -1,170 +1,191 @@
 export const defaultDimensionData = [
   {
-    definition: "Default definition 1",
+    definition: "Nature and purpose of learning tasks",
     type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement:
+      "Learning tasks reflect the practical context in which the knowledge or skills will be used.",
+    rightCardStatement:
+      "Learning tasks focus on theoretical concepts (even if physical objects are used).",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Academic/abstract" },
+      { position: 100, label: "Authentic/experimental" },
     ],
   },
   {
-    definition: "Default definition 2",
+    definition: "Degree of structure of learning tasks",
     type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement: "Learners can only do learning tasks in one way.",
+    rightCardStatement:
+      "Learners can do the learning tasks in a variety of ways.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "High/constrained" },
+      { position: 100, label: "Low/open" },
     ],
   },
   {
-    definition: "Default definition 3",
+    definition: "Interactivity",
     type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement: "Learners can contribute to or change the content.",
+    rightCardStatement:
+      "The content is pre-prepared and fixed. Learners can't contribute to or change the content.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Navigational" },
+      { position: 100, label: "Manipulative" },
     ],
   },
   {
-    definition: "Default definition 4",
+    definition: "Learning framework",
     type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement:
+      "Learners perform set learning tasks using the information provided.",
+    rightCardStatement:
+      "Learners explore the content and learning tasks in their own way.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Structured" },
+      { position: 50, label: "Guided" },
+      { position: 100, label: "Facilitated" },
     ],
   },
   {
-    definition: "Default definition 5",
+    definition: "Learning control",
     type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement:
+      "Learning activities are directed by the teacher or resource.",
+    rightCardStatement:
+      "Students choose the sequence and type of learning activities they undertake.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Teacher-managed" },
+      { position: 100, label: "Student-managed" },
     ],
   },
   {
-    definition: "Default definition 6",
+    definition: "Learning process",
     type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement: "Students reproduce accepted discipline knowledge.",
+    rightCardStatement:
+      "Students learn to construct their own understanding of the discipline.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Reproduction" },
+      { position: 100, label: "Construction" },
     ],
   },
   {
-    definition: "Default definition 7",
+    definition: "Feedback",
     type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
-    leftCardDefaultSliderPosition: 0,
-    rightCardDefaultSliderPosition: 100,
+    leftCardStatement: "Left card statement", // TODO: TBC
+    rightCardStatement: "Right card statement", // TODO: TBC
+    leftCardDefaultSliderPosition: 0, // TODO: TBC
+    rightCardDefaultSliderPosition: 100, // TODO: TBC
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Minimal" },
+      { position: 50, label: "Fixed" },
+      { position: 100, label: "Responsive" },
     ],
   },
   {
-    definition: "Default definition 8",
-    type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
-    leftCardDefaultSliderPosition: 0,
-    rightCardDefaultSliderPosition: 100,
-    marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
-    ],
-  },
-  {
-    definition: "Default definition 9",
-    type: "Practice",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
-    leftCardDefaultSliderPosition: 0,
-    rightCardDefaultSliderPosition: 100,
-    marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
-    ],
-  },
-  {
-    definition: "Default definition 10",
+    definition: "Learning goals (Curriculum beliefs)",
     type: "Beliefs",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement:
+      "Learning goals are clearly defined and can be linked to assessment clearly.",
+    rightCardStatement:
+      "Learning goals are open to interpretation and are not easily assessable.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Sharply-focused" },
+      { position: 100, label: "Unfocused" },
     ],
   },
   {
-    definition: "Default definition 11",
+    definition: "Student collaboration (Curriculum beliefs)",
     type: "Beliefs",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
-    leftCardDefaultSliderPosition: 0,
-    rightCardDefaultSliderPosition: 100,
+    leftCardStatement: "Left card statement", // TODO: TBC
+    rightCardStatement: "Right card statement", // TODO: TBC
+    leftCardDefaultSliderPosition: 0, // TODO: TBC
+    rightCardDefaultSliderPosition: 100, // TODO: TBC
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Minimal" },
+      { position: 50, label: "Social" },
+      { position: 100, label: "Cognitive" },
     ],
   },
   {
-    definition: "Default definition 12",
+    definition: "Dimension 10", // TODO: TBC
     type: "Beliefs",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement:
+      "The curriculum or course content determines the learning goals of this course.",
+    rightCardStatement:
+      "Learners can determine (some or all of) the learning outcomes in the course.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Instructivist" },
+      { position: 100, label: "Constructivist" },
     ],
   },
   {
-    definition: "Default definition 13",
+    definition: "Dimension 11", // TODO: TBC
     type: "Beliefs",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
-    leftCardDefaultSliderPosition: 0,
+    leftCardStatement:
+      "Learners' incoming knowledge of the content is taken into account by explaining course concepts in ways they are likely to understand.",
+    rightCardStatement:
+      "Learners' incoming knowledge is taken into account through dialogue and discussion between learners and teachers.",
+    leftCardDefaultSliderPosition: 50,
     rightCardDefaultSliderPosition: 100,
     marks: [
       { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 50, label: "Pre-emptive" },
+      { position: 100, label: "Conversational" },
     ],
   },
   {
-    definition: "Default definition 14",
+    definition: "Dimension 12", // TODO: TBC
     type: "Beliefs",
-    leftCardStatement: "left definition",
-    rightCardStatement: "right definition",
+    leftCardStatement: "Left card statement", // TODO: TBC
+    rightCardStatement: "Right card statement", // TODO: TBC
+    leftCardDefaultSliderPosition: 0, // TODO: TBC
+    rightCardDefaultSliderPosition: 100, // TODO: TBC
+    marks: [
+      { position: 0, label: "Left Mark" }, // TODO: TBC
+      { position: 100, label: "Right Mark" }, // TODO: TBC
+    ],
+  },
+  {
+    definition: "Dimension 13", // TODO: TBC
+    type: "Beliefs",
+    leftCardStatement: "Left card statement", // TODO: TBC
+    rightCardStatement: "Right card statement", // TODO: TBC
+    leftCardDefaultSliderPosition: 0, // TODO: TBC
+    rightCardDefaultSliderPosition: 100, // TODO: TBC
+    marks: [
+      { position: 0, label: "Left Mark" }, // TODO: TBC
+      { position: 100, label: "Right Mark" }, // TODO: TBC
+    ],
+  },
+  {
+    definition: "Dimension 14", // TODO: TBC
+    type: "Beliefs",
+    leftCardStatement:
+      "For learning to occur in this course, the learning needs to happen 'solo', in private.",
+    rightCardStatement:
+      "For learning to occur in this course, the learning needs to happen together, in a learning community.",
     leftCardDefaultSliderPosition: 0,
     rightCardDefaultSliderPosition: 100,
     marks: [
-      { position: 0, label: "Left Mark" },
-      { position: 100, label: "Right Mark" },
+      { position: 0, label: "Personal" },
+      { position: 100, label: "Social" },
     ],
   },
 ];
