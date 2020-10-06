c4fad2e251431475a856792f3e62d34c0d7a5ef1
Gargi Bansal <gargi.bansal@gmail.com>
2020-10-07 09:49:15 +1300

    feat(front end): changed styling and title text
    changed styling and title text

--- src/pages/PlayReason.tsx
+++ src/pages/PlayReason.tsx
@@ -75,9 +75,7 @@ const PlayReason: React.FC<RouteComponentProps> = (props) => {
 
       <div className="Main-Container">
         <div className="Reason-Container">
-          <Typography className="Heading-Text">
-            Create Chart for {courseName}
-          </Typography>
+          <Typography className="Heading-Text">Create a New Chart</Typography>
           <div className="Reason-Input-Container">
             <Typography className="Subheading-Text">
               Why are you playing the HeaRT game?
