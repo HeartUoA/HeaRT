7f3d749040259b0eca7bc403b1cab9b1d0fd0787
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2020-10-07 09:08:00 +1300

    feat(playreason): fixed up page styling

--- src/pages/PlayReason.tsx
+++ src/pages/PlayReason.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { withRouter, RouteComponentProps, useParams } from "react-router-dom";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import * as QueryString from "query-string";
 
-import { Button, Layout, Select, Typography, Radio } from "antd";
+import { Button, Typography, Radio } from "antd";
 
 import Header from "../components/Header";
 import "../styles/PlayReason.css";
@@ -12,16 +12,11 @@ import TextArea from "antd/lib/input/TextArea";
 import { createCourse } from "../types/course";
 import { API_DOMAIN } from "../config";
 
-interface ParamTypes {
-  chartID: string;
-}
-
 const NONE_SELECTED = "A reason for playing the HeaRT Game must be selected";
 const NO_TEXT = "Please type in a reason for playing the HeaRT Game";
 
 const PlayReason: React.FC<RouteComponentProps> = (props) => {
   const [cookies] = useCookies(["accessToken"]);
-  const { chartID } = useParams<ParamTypes>();
   const [state, setState] = useState("");
   const [reason, setReason] = useState("");
   const [error, setError] = useState("");
@@ -75,15 +70,15 @@ const PlayReason: React.FC<RouteComponentProps> = (props) => {
   };
 
   return (
-    <div className="Course-Info">
+    <div className="Play-Reason">
       <Header />
 
-      <Layout.Content className="Main-Container">
-        <div className="Form-Container">
+      <div className="Main-Container">
+        <div className="Reason-Container">
           <Typography className="Heading-Text">
             Create Chart for {courseName}
           </Typography>
-          <div>
+          <div className="Reason-Input-Container">
             <Typography className="Subheading-Text">
               Why are you playing the HeaRT game?
             </Typography>
@@ -105,7 +100,7 @@ const PlayReason: React.FC<RouteComponentProps> = (props) => {
               </Radio>
             </Radio.Group>
             <TextArea
-              className="Play-Reason"
+              className="Reason-Text"
               rows={3}
               placeholder="Enter reason here..."
               onChange={(e) => {
@@ -119,14 +114,14 @@ const PlayReason: React.FC<RouteComponentProps> = (props) => {
           <div className="Button-Container">
             <Button
               type="primary"
-              className="Nav-Button"
+              className="Cancel-Button"
               onClick={onCancelClick}
             >
               <Typography className="Button-Font">Cancel</Typography>
             </Button>
             <Button
               type="primary"
-              className="Nav-Button"
+              className="Confirm-Button"
               onClick={onConfirmClick}
             >
               <Typography className="Button-Font">Confirm</Typography>
@@ -138,7 +133,7 @@ const PlayReason: React.FC<RouteComponentProps> = (props) => {
             )}
           </div>
         </div>
-      </Layout.Content>
+      </div>
     </div>
   );
 };
