7d53d9a97371bee9f803f1d16be34a23c278e698
Michelle Tan <michellemtan@outlook.com>
2020-10-07 14:45:16 +1300

    feat(chart controller): change put request

--- server/chart/chart.model.js
+++ server/chart/chart.model.js
@@ -11,7 +11,7 @@ const chartSchemaDef = {
   },
   reasonOfPlay: {
     type: String,
-    required: true,
+    required: false,
   },
 };
 
--- server/course/course.controller.js
+++ server/course/course.controller.js
@@ -146,4 +146,46 @@ router
     }
   });
 
+router
+  .route("/:courseID/chart/:chartID")
+  .put(bodyParser.json(), authenticateJWT, async (request, response) => {
+    try {
+      const chart = await Chart.find({ _id: request.params.chartID }, function (
+        err,
+        result
+      ) {
+        if (!result || result.length === 0) {
+          return response
+            .status(404)
+            .send(
+              "No chart found. You can create one at /api/course/{courseID}/chart/"
+            );
+        }
+        if (err) {
+          return response.status(400).send(err);
+        }
+      });
+
+      const charts = await Chart.find({ courseID: request.params.courseID });
+      if (!charts || charts.length === 0) {
+        return response.status(404).send("No charts found");
+      }
+
+      const updatedChart = {};
+
+      updatedChart.reasonOfPlay =
+        request.body.reasonOfPlay !== undefined
+          ? request.body.reasonOfPlay
+          : chart.reasonOfPlay;
+
+      await Chart.findByIdAndUpdate(
+        { _id: request.params.chartID },
+        updatedChart
+      );
+      return response.status(200).json(updatedChart);
+    } catch (error) {
+      return response.status(400).send(error);
+    }
+  });
+
 export default router;
